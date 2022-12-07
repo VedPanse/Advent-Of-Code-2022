e9dcc2a1e229ad9f2648f52cbe3c59b6cf1ac4a1
Ved Panse <75316092+VedPanse@users.noreply.github.com>
2022-12-07 06:36:28 +0530

    Update day6-Part2.py

--- day6/day6-Part2.py
+++ day6/day6-Part2.py
@@ -5,7 +5,7 @@ fs.close()
 
 def func():
     for ind in range(0, len(data)):
-        lst = [data[ind], data[ind + 1], data[ind + 2], data[ind + 3], data[ind + 4], data[ind + 5], data[ind + 6], data[ind + 7], data[ind + 8], data[ind + 9], data[ind + 10], data[ind + 11], data[ind + 12], data[ind + 13]]
+        lst = [data[ind + k] for k in range(14)]
         sets = list(set(lst))
         lst.sort()
         sets.sort()
